fa135ee8df92808bddb2d56dcecbb908e4c901f1
Tarsis <br.tmvdl@gmail.com>
2024-04-04 20:19:43 -0300

    Updating app files

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,19 @@
+const WebSocket = require('ws')
 
+const client = new WebSocket(require('./config.js').url)
+
+client.on('open', function open() {
+  console.log('Conected')
+})
+
+client.on('message', function incoming(data) {
+  console.log(data)
+})
+
+client.on('error', function () {
+  console.log('Error')
+})
+
+client.on('close', function close() {
+  console.log('Connection Closed')
+})
