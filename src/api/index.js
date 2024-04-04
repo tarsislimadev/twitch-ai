d9591cc01e5a3b5d0ea438adc9c9fc4d1349a501
Tarsis <br.tmvdl@gmail.com>
2024-04-04 20:50:54 -0300

    Updating app files

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,1 @@
 const { ee } = require('./twitch.js')
-
-ee.on('open', () => console.log('open', Date.now()))
-
-ee.on('close', () => console.log('close', Date.now()))
--- src/api/twitch.js
+++ src/api/twitch.js
@@ -8,11 +8,23 @@ const client = new WebSocket(config.url)
 
 const ee = new EventEmitter()
 
+ee.on('request', (data) => [console.log('request', Date.now(), data), client.send(JSON.stringify(data))])
+
+setInterval(() => ee.emit('request', { 'type': 'PING' }), 30 * 1000)
+
+ee.on('open', () => console.log('open', Date.now()))
+
 client.on('open', () => ee.emit('open'))
 
-client.on('message', (data) => ee.emit('message', data))
+ee.on('response', (data) => console.log('response', Date.now(), data.toString()))
+
+client.on('message', (data) => ee.emit('response', data))
+
+ee.on('error', (err) => console.log('error', Date.now(), err))
 
 client.on('error', (err) => ee.emit('error', err))
+
+ee.on('close', () => console.log('close', Date.now()))
 
 client.on('close', () => ee.emit('close'))
 
